ccf740c4ff35b68fafae8416b03761123e46af73
alison-acuna <aacuna136@gmail.com>
2016-06-09 11:31:53 -0700

    added api display

--- communitycrmapp/views.py
+++ communitycrmapp/views.py
@@ -15,29 +15,6 @@ def home(request):
 def searchpage(request):
     return render(request, 'communitycrmapp/searchpage.html', {})
 
-def meetups(request, id):
-    member = Member.objects.get(pk=id)
-    meetup_id = member.meetupid
-    r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
-    meetups = r.json()
-    return render(request, 'communitycrmapp/meetups.html', {
-        'meetups': meetups
-    })
-
-def member_item(request, id):
-    member = Member.objects.get(pk=id)
-    meetup_id = member.meetupid
-    r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
-    meetups = (r.json()['topics'])
-    li = []
-    topics = (r.json()['topics'])
-    for topic in topics:
-        li.append(topic['name'])
-    return render(request, 'communitycrmapp/member.html', {
-        'member': member,
-        'meetups': li
-    })
-
 def new(request):
     if request.method == "POST":
         form = MemberForm(request.POST)
@@ -49,10 +26,54 @@ def new(request):
         form = MemberForm()
     return render(request, 'communitycrmapp/new.html', {'form': form})
 
+def meetups(request, id):
+    member = Member.objects.get(pk=id)
+    # meetups pull starts here
+    meetup_id = member.meetupid
+    r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+    meetups = (r.json()['topics'])
+    li = []
+    topics = (r.json()['topics'])
+    for topic in topics:
+        li.append(topic['name'])
+    # meetups pull ends here
+    return render(request, 'communitycrmapp/meetups.html', {
+        'meetups': meetups
+    })
+
+def member_item(request, id):
+    member = Member.objects.get(pk=id)
+    # meetups pull starts here
+    meetup_id = member.meetupid
+    r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+    meetups = (r.json()['topics'])
+    li = []
+    topics = (r.json()['topics'])
+    for topic in topics:
+        li.append(topic['name'])
+    # meetups pull ends here
+    return render(request, 'communitycrmapp/member.html', {
+        'member': member,
+        'meetups': li,
+        'engagement_score': engagement_score
+    })
+
+
 def display(request):
     members = Member.objects.all()
+    for member in members:
+        # meetups pull starts here
+        meetup_id = member.meetupid
+        r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+        meetups = (r.json()['topics'])
+        li = []
+        topics = (r.json()['topics'])
+        for topic in topics:
+            li.append(topic['name'])
+        # meetups pull ends here
     return render(request, 'communitycrmapp/display.html', {
-        'members': members
+        'members': members,
+        'meetups': li
     })
 
 def search_by_name(request):
@@ -60,9 +81,19 @@ def search_by_name(request):
         search_id = request.POST.get('namequery')
         try:
             member_by_name = Member.objects.filter(name__icontains= search_id)
-            print(member_by_name)
+            for member in member_by_name:
+                # meetups pull starts here
+                meetup_id = member.meetupid
+                r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+                meetups = (r.json()['topics'])
+                li = []
+                topics = (r.json()['topics'])
+                for topic in topics:
+                    li.append(topic['name'])
+                # meetups pull ends here
             return render(request, 'communitycrmapp/results.html', {
-                'member_by_name': member_by_name})
+                'member_by_name': member_by_name,
+                'meetups': li})
         except Member.DoesNotExist:
             return HttpResponse("no such user")
     else:
@@ -73,8 +104,20 @@ def search_by_volunteer(request):
         search_id = request.POST.get('volunteerquery')
         try:
             member_by_volunteer = Member.objects.filter(volunteer__exact= search_id)
+            for member in member_by_volunteer:
+                # meetups pull starts here
+                meetup_id = member.meetupid
+                r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+                meetups = (r.json()['topics'])
+                li = []
+                topics = (r.json()['topics'])
+                for topic in topics:
+                    li.append(topic['name'])
+                # meetups pull ends here
             return render(request, 'communitycrmapp/results.html', {
-                'member_by_volunteer': member_by_volunteer})
+                'member_by_volunteer': member_by_volunteer,
+                'meetups': li
+                })
         except Member.DoesNotExist:
             return HttpResponse("no such user")
     else:
@@ -85,8 +128,19 @@ def search_by_host(request):
         search_id = request.POST.get('hostquery')
         try:
             member_by_host = Member.objects.filter(host__gte= search_id)
+            for member in member_by_host:
+                # meetups pull starts here
+                meetup_id = member.meetupid
+                r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+                meetups = (r.json()['topics'])
+                li = []
+                topics = (r.json()['topics'])
+                for topic in topics:
+                    li.append(topic['name'])
+                # meetups pull ends here
             return render(request, 'communitycrmapp/results.html', {
-                'member_by_host': member_by_host})
+                'member_by_host': member_by_host,
+                'meetups': li})
         except Member.DoesNotExist:
             return HttpResponse("no such user")
     else:
@@ -121,8 +175,19 @@ def search_by_fb(request):
         search_id = request.POST.get('fbquery')
         try:
             member_by_fb = Member.objects.filter(fbgroupmember__exact= search_id)
+            for member in member_by_fb:
+                # meetups pull starts here
+                meetup_id = member.meetupid
+                r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+                meetups = (r.json()['topics'])
+                li = []
+                topics = (r.json()['topics'])
+                for topic in topics:
+                    li.append(topic['name'])
+                # meetups pull ends here
             return render(request, 'communitycrmapp/results.html', {
-                'member_by_fb': member_by_fb})
+                'member_by_fb': member_by_fb,
+                'meetups': li})
         except Member.DoesNotExist:
             return HttpResponse("no such user")
     else:
@@ -133,8 +198,19 @@ def donor_search(request):
         search_id = request.POST.get('donationquery')
         try:
             donor = Member.objects.filter(donationtotalammount__gte= search_id)
+            for member in donor:
+                # meetups pull starts here
+                meetup_id = member.meetupid
+                r = requests.get("{}{}{}".format('https://api.meetup.com/members/', meetup_id, '/?key=68541e621567da7c512e2f6912517&sign=true&fields=topics'))
+                meetups = (r.json()['topics'])
+                li = []
+                topics = (r.json()['topics'])
+                for topic in topics:
+                    li.append(topic['name'])
+                # meetups pull ends here
             return render(request, 'communitycrmapp/results.html', {
-                'donor': donor})
+                'donor': donor,
+                'meetups': li})
         except Member.DoesNotExist:
             return HttpResponse("no such user")
     else:
